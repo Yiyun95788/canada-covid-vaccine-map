a916d076b0a0131b20f80238967d542a06cd6c14
Yiyun95788 <yiyunzhang957@gmail.com>
2024-11-01 20:58:24 -0400

    Fix issues of title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CanadaMap from './CanadaMap';
+import './App.css';
 
 function App() {
-  return (
-      <div>
-        <h1>Canada COVID-19 Vaccine Map</h1>
-        <CanadaMap />
-      </div>
-  );
+    return (
+        <div className="map-container">
+            <h1 className="map-title">Canada COVID-19 Vaccine Map</h1>
+            <CanadaMap />
+        </div>
+    );
 }
 
 export default App;
